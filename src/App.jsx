c101747161532681e6c31701809e811ae8c32679
gbZyuuu <gabrielws12345@gmail.com>
2025-08-23 21:42:19 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   const [error, setError] = useState('')
   
   // Inicializa o PocketBase
-  const pb = new PocketBase('https://SEU-PROJETO.pages.dev/api')
+  const pb = new PocketBase('http://pocketbase:8090');
   
   // Configuração de juros
   const INTEREST_RATE = 0.018; // 1.8% ao mês
